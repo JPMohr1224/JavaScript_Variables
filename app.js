3f3c5ab3c78d9f998f72eccde0ab9a81a9e9f2e4
JP Mohr <James.Mohr2.0@hotmail.com>
2024-06-05 15:24:12 -0400

    JSVaribles

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ console.log(
 
 // Exercise 1
 
+let lastNAme;
+var age;
+
+
 // Exercise 2
 
+const firstName = "JP";
+lastName = "Mohr";
+age = 50;
+
 // Exercise 3
+
+let language = "JavaScript";
+let yearCreated = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let onSale = false;
+let salePricing = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
